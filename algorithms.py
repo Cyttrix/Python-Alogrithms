71ab6bfcd4dcb34f7f7f3509e519a1fdfad18bad
Ari Meyer <ari.f.meyer@gmail.com>
2024-01-31 17:59:14 -0600

    1st task complete

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+
+
+def task_1(n):
+    if n % 2 == 0:
+        print("Is " + str(n) + " Even?")
+        return True
+    else: 
+        print("Is " + str(n) + " Odd?")
+        return True
+        
+
+value = task_1(4)
+print(value)
+
